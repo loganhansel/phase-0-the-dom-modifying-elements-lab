01992147e026c1f1f97bae29d0c52981a30c594d
loganhansel <loganhansel23@gmail.com>
2022-05-17 16:07:05 -0400

    commit tree 25df0f50d08e0249405aaffdfff09e44fdff42ff

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const mainNode = document.getElementById('main');
+mainNode.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Logan is the champion";
+document.body.append(newHeader);
